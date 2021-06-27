0fed39ac5154d53b895374c0d09b8f36eb47383e
Martinpthomas <86546450+Martinpthomas@users.noreply.github.com>
2021-06-27 13:48:24 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import Header from"./Header";
+import Footer from"./Footer";
+import Info frim"./Info";
+
+function App() {
+
+return ( )
+}
+<div>
+
+<Header
+
+</div>
+
+);
+}
+export default App;
